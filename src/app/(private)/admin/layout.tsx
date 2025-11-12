1c2f7b4d82669bbac5a823d0971cb17a7e1c7052
Mihail <gamerbg5505@gmail.com>
2025-11-13 01:36:03 +0200

    d

--- src/app/(private)/admin/layout.tsx
+++ src/app/(private)/admin/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
-import { auth } from "@/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
@@ -20,22 +17,6 @@ export default async function AdminLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const session = await auth();
-    const headersList = await headers();
-    const path = headersList.get("x-pathname") || "/";
-
-    if (!session && path !== "/admin/login") {
-        redirect("/admin/login");
-    }
-
-    if (session?.user?.role !== "ADMIN") {
-        redirect("/");
-    }
-
-    if (session?.user && path === "/admin/login") {
-        redirect("/admin/dashboard");
-    }
-
     return (
         <html lang="bg">
             <body className={inter.className}>
